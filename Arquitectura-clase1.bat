f127ad221606ee86eb9e1b366013350f300bf99b
Disble <decoder@hotmail.es>
2018-04-17 00:26:04 -0500

    feat: add workshop 1

---
+++ Arquitectura-clase1.bat
@@ -0,0 +1,102 @@
+@echo off
+:menu
+cls
+echo              -----------------------------------------------------
+echo              -                 MENU PRINCIPAL                    -
+echo              -----------------------------------------------------
+echo              -   1.- SABER DIRECCION IP                          -
+echo              -   2.- ABRIR ARCHIVO                               -
+echo              -   3.- OPERACION SUMA EN ENSAMBLADOR               -
+echo              -   4.- OPERACION RESTA ENSAMBLADOR                 -
+echo              -   5.- OPERACOPM MULTIPLICACION EN ENSAMBLADOR     -
+echo              -   6.- OPERACOPM DIVISION EN ENSAMBLADOR           -
+echo              -   7.- SALIR                                       -
+echo              -----------------------------------------------------
+echo.               
+set /p                 ver=Que desea hacer ?
+if %ver%==1 goto  IP
+if %ver%==2 goto  AbrirArchivo
+if %ver%==3 goto  Suma
+if %ver%==4 goto  Resta
+if %ver%==5 goto  Multiplicacion
+if %ver%==6 goto  Division
+if %ver%==7 goto  Salir
+
+:IP
+echo.
+echo.
+echo Implementar IP
+echo.
+pause >nul
+cd \
+ipconfig
+echo.
+echo.
+echo PRESIONE ENTER PARA INGRESAR AL MENU
+pause >nul 
+goto menu
+
+:AbrirArchivo
+echo.
+echo.
+echo Implementar Abrir Archivo
+echo.
+pause >nul
+type Arquitectura-clase1.bat
+echo.
+echo.
+echo PRESIONE ENTER PARA INGRESAR AL MENU
+pause >nul 
+goto menu
+
+:Suma
+echo.
+echo.
+pause >nul
+cd \
+include 'em8086.inc'
+echo.
+echo.
+echo PRESIONE ENTER PARA REGRESAR AL MENU
+pause >nul
+goto menu
+
+:Resta
+echo.
+echo.
+echo Implementar resta ENSAMBLADOR
+echo.
+set /p                 x=numero 1: 
+set /p                 y=numero 2: 
+set /p %z%=x + y
+
+echo.
+echo Resultado: %z%
+echo.
+echo PRESIONE ENTER PARA REGRESAR AL MENU
+pause >nul
+goto menu
+
+:Multiplicacion
+echo.
+echo.
+echo Implementar multiplicacion ENSAMBLADOR
+echo.
+echo PRESIONE ENTER PARA REGRESAR AL MENU
+pause >nul
+goto menu
+
+:Division
+echo.
+echo.
+echo Implementar division ENSAMBLADOR
+echo.
+echo PRESIONE ENTER PARA REGRESAR AL MENU
+pause >nul
+goto menu
+
+:Salir
+echo.
+echo.
+echo Adios
+exit
