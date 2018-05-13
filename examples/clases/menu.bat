ba3b78e351a2f945d5ec64cbccaec03a48c1148a
Disble <decoder@hotmail.es>
2018-05-13 00:24:32 -0500

    feat: new option for menu.bat

--- examples/clases/menu.bat
+++ examples/clases/menu.bat
@@ -1,40 +1,41 @@
 @echo off
 
-::IF %parametro%==1 (
-::	goto  IP
-::	)
+:: control de parametros
+if "%1"=="" goto menu
 set parametro=%1
-echo %parametro%
-if %parametro%==1 goto  IP
-if %parametro%==2 goto  AbrirArchivo
-if %parametro%==3 goto  Suma
-if %parametro%==4 goto  Resta
-if %parametro%==5 goto  Multiplicacion
-if %parametro%==6 goto  Division
-if %parametro%==7 goto  Salir
+if %parametro%==1 goto IP
+if %parametro%==2 goto DOSBox
+if %parametro%==3 goto AbrirArchivo
+if %parametro%==4 goto Suma
+if %parametro%==5 goto Resta
+if %parametro%==6 goto Multiplicacion
+if %parametro%==7 goto Division
+if %parametro%==8 goto Salir
 
 :menu
-::cls
+cls
 echo              -----------------------------------------------------
 echo              -                 MENU PRINCIPAL                    -
 echo              -----------------------------------------------------
 echo              -   1.- SABER DIRECCION IP                          -
-echo              -   2.- ABRIR ARCHIVO                               -
-echo              -   3.- OPERACION SUMA EN ENSAMBLADOR               -
-echo              -   4.- OPERACION RESTA EN ENSAMBLADOR              -
-echo              -   5.- OPERACION MULTIPLICACION EN ENSAMBLADOR     -
-echo              -   6.- OPERACION DIVISION EN ENSAMBLADOR           -
-echo              -   7.- SALIR                                       -
+echo              -   2.- ABRIR DOSBOX			                          -
+echo              -   3.- ABRIR ARCHIVO                               -
+echo              -   4.- OPERACION SUMA EN ENSAMBLADOR               -
+echo              -   5.- OPERACION RESTA EN ENSAMBLADOR              -
+echo              -   6.- OPERACION MULTIPLICACION EN ENSAMBLADOR     -
+echo              -   7.- OPERACION DIVISION EN ENSAMBLADOR           -
+echo              -   8.- SALIR                                       -
 echo              -----------------------------------------------------
 echo.               
 set /p                 ver=Que desea hacer? 
 if %ver%==1 goto  IP
-if %ver%==2 goto  AbrirArchivo
-if %ver%==3 goto  Suma
-if %ver%==4 goto  Resta
-if %ver%==5 goto  Multiplicacion
-if %ver%==6 goto  Division
-if %ver%==7 goto  Salir
+if %ver%==2 goto  DOSBox
+if %ver%==3 goto  AbrirArchivo
+if %ver%==4 goto  Suma
+if %ver%==5 goto  Resta
+if %ver%==6 goto  Multiplicacion
+if %ver%==7 goto  Division
+if %ver%==8 goto  Salir
 
 :IP
 echo.
@@ -49,16 +50,22 @@ echo PRESIONE ENTER PARA INGRESAR AL MENU
 pause >nul 
 goto menu
 
-:AbrirArchivo
+:DOSBox
 echo.
 echo.
 echo Abriendo DOSBox...
 echo.
-::pause >nul
-::more Arquitectura-clase1.bat
 SET PATH=%PATH%;"C:\Program Files (x86)\DOSBox-0.74"
 dosbox.exe -c "MOUNT C C:/DOSBOX/MASM611/BINR" -c "C:" -c "OPERABIN" -c "exit"
 goto Salir
+
+:AbrirArchivo
+echo.
+echo.
+echo Implementar Abrir Archivo
+echo.
+pause >nul
+more menu.bat
 echo.
 echo.
 echo PRESIONE ENTER PARA INGRESAR AL MENU
@@ -66,10 +73,6 @@ pause >nul
 goto menu
 
 :Suma
-::cd \
-::include 'em8086.inc'
-::echo.
-::echo.
 echo.
 echo.
 echo Implementar suma ENSAMBLADOR
